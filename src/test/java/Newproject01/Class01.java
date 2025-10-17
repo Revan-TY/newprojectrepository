535a6ec9c9e52bca7cbd58ca88550239f00e2f12
likithbg <likithbg1234@gmail.com>
2025-10-17 18:17:34 +0530

    commit3

--- src/test/java/Newproject01/Class01.java
+++ src/test/java/Newproject01/Class01.java
@@ -1,8 +1,10 @@
 package Newproject01;
 
-public class Class01 {
+public class Class01 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 
 		System.out.println("Hello");
